0dbab458eb4bafb04c481c8a59c553ecb608cadc
bysolanabdi-lab <bysolanabdi@gmail.com>
2026-07-02 14:10:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { generateStaticHtmlPlugin } from "./vite-plugins/generate-static-html.ts";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths(),
-    generateStaticHtmlPlugin()
+    tsconfigPaths()
   ],
   build: {
-    // Changement ici : on vise la racine du dossier de build standard
     outDir: "dist", 
   }
 });
